272cbf798837e46bf0fe017241c20f4d1ccbcef0
diederikb <beckersdiederik@gmail.com>
2024-02-18 16:46:47 -0800

    added file with non-rl policies

---
+++ non_rl_policies.py
@@ -0,0 +1,29 @@
+# non_rl_policies.py >
+
+class PID:
+    def __init__(self, Kp, Ki, Kd, Ts):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.Ts = Ts
+        self._integral_error = 0.0
+        self._previous_error = 0.0
+    
+    def predict(self, observations, **kwargs):
+        assert len(observations[0]) == 1, "observations should contain only one value, i.e., the error"
+        fy_error = observations[0]
+        self._integral_error += fy_error * self.Ts
+        self._error_derivative = (fy_error - self._previous_error) / self.Ts
+        self._previous_error = fy_error
+        action = self.Kp * fy_error + self.Ki * self._integral_error + self.Kd * self._error_derivative
+        return [action], []
+    
+class PrescribedAction:
+    def __init__(self, alpha_ddot_prescribed):
+        self.alpha_ddot_prescribed = alpha_ddot_prescribed
+        self.step = 0
+    
+    def predict(self, observations, **kwargs):
+        action = self.alpha_ddot_prescribed[self.step]
+        self.step += 1
+        return [action], []
